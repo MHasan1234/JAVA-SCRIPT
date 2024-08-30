ef14a59b4f4ab2e82d7c18bbd3201bbc012891c0
MHasan1234 <160541622+MHasan1234@users.noreply.github.com>
2024-08-31 00:22:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+//  console.log("Hello World!");
+//  console.log("Apna College");
+ //let a = 10;
+ //let b = 5;
+ //console.log("sum is : ", a + b);
+ //this is a comment 
+
+//TEMPLATE LITERALS 
+ //let pencilPrice = 10;
+ //let eraserPrice = 5;
+ // let output = ="The total price : " + (pencilPrice + erasorPrice) + " Rupees.";
+ //console.log(`The total price is : ${pencilPrice + eraserPrice} Rupees.`); 
+
+
+ //Arithmetic Operators
+
+//  let a = 10;
+//  let b = 5;
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(a/b);
+//  console.log(a**b);
+//  console.log(a++);
+//  console.log(++a);
+ 
+
+//Operators
+// let age = 18;
+// console.log(age > 18);
+
+// let n = 5;
+// let str = '5';
+// typeof n;
+// typeof str;
+// n === str; //datatype comparsion
+
+// 123=="123";
+
+//Conditional Statements
+// console.log("before my if statement");
+// let age = 23;
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+// console.log("after my if statement");
+// if (age > 20){
+//     console.log("you are in your 20s");
+// }
+
+let firstName = "shradha";
+if (firstName == "shradha") {
+    console.log(`Welcome ${firstName}`);
+}
